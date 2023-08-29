e0f019ec2b96e358453cb1837675f940bfa9c555
juhun-park <juhun0617@gmail.com>
2023-08-29 10:31:39 +0900

    리사이클러뷰로 리스트 제작 및 아이템데이터 클래스 구축

---
+++ app/src/main/java/com/example/localfoodoasis/MyAdapter.java
@@ -0,0 +1,91 @@
+package com.example.localfoodoasis;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private List<ItemData> mDataList;
+
+    // 어댑터 생성자
+    public MyAdapter(List<ItemData> list) {
+        this.mDataList = list;
+    }
+
+    // 뷰홀더 정의
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView imageView;
+        public TextView price, maker, address, day, number;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.list_image);
+            price = itemView.findViewById(R.id.price);
+            maker = itemView.findViewById(R.id.maker);
+            address = itemView.findViewById(R.id.address);
+            day = itemView.findViewById(R.id.day);
+            number = itemView.findViewById(R.id.number);
+        }
+    }
+
+    // 새로운 뷰 홀더 생성
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.my_list_view, parent, false);
+        return new ViewHolder(v);
+    }
+
+    // 뷰 홀더에 데이터 바인딩
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        ItemData item = mDataList.get(position);
+        holder.imageView.setImageResource(item.imageRes);
+        holder.price.setText(item.price);
+        holder.maker.setText(item.maker);
+        holder.address.setText(item.address);
+        holder.day.setText(item.day);
+        holder.number.setText(item.number);
+    }
+
+    // 데이터 항목의 개수 반환
+    @Override
+    public int getItemCount() {
+        return mDataList.size();
+    }
+
+
+
+
+    public static class ItemData {
+        private String price;
+        private String maker;
+        private String address;
+        private String day;
+        private String number;
+        private int imageRes;
+
+        // 생성자
+        public ItemData(String price, String maker, String address, String day, String number, int imageRes) {
+            this.price = price;
+            this.maker = maker;
+            this.address = address;
+            this.day = day;
+            this.number = number;
+            this.imageRes = imageRes;
+        }
+
+    }
+
+
+
+
+
+}
+
+
--- app/src/main/java/com/example/localfoodoasis/watermelon.java
+++ app/src/main/java/com/example/localfoodoasis/watermelon.java
@@ -1,14 +1,44 @@
 package com.example.localfoodoasis;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class watermelon extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_watermelon);
+        Spinner spinner = findViewById(R.id.spiner);
+        String[] location = {"장성로컬푸드직매장","농협하나로광천점"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.sppiner_item,location);
+        spinner.setAdapter(adapter);
+
+        List<MyAdapter.ItemData> myDataList = new ArrayList<>();
+
+        MyAdapter.ItemData item1 = new MyAdapter.ItemData("18,000원","박주훈","전라남도 장성군 남면","2023-08-23","010-1234-5678",R.drawable.melon);
+        myDataList.add(item1);
+
+        RecyclerView recyclerView = findViewById(R.id.listview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new MyAdapter(myDataList));
+
+
     }
+
+
+
+
+
 }
+
